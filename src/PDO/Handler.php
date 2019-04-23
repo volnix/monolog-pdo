1fd19eeb4c31902f8607c0015c68aec7fcda92aa
Nick Volgas <nvolgas@volnix.local>
2019-04-23 13:15:21 -0500

    Initial commit.

---
+++ src/PDO/Handler.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace volnix\Monolog\PDO;
+
+use Monolog\Logger;
+use Monolog\Handler\AbstractProcessingHandler;
+use PDO;
+
+class Handler extends AbstractProcessingHandler
+{
+	/**
+	 * @var \PDO
+	 */
+	private $pdo;
+
+	/**
+	 * @var string
+	 */
+	private $logs_table;
+
+	/**
+	 * @var string $default_logs_table_name - 'logs'
+	 */
+	const DEFAULT_TABLE_NAME = 'logs';
+
+	/**
+	 * SQL constructor.
+	 * @param \PDO $pdo
+	 * @param string $logs_table
+	 */
+	public function __construct(\PDO $pdo, string $logs_table = self::DEFAULT_TABLE_NAME)
+	{
+		$this->pdo = $pdo;
+		$this->logs_table = $logs_table;
+	}
+
+	/**
+	 * @param array $data
+	 * @return void
+	 * @throws \Exception
+	 */
+	protected function write(array $data) : void
+	{
+		$fields = [
+			'level'     => $data['level_name'],
+			'channel'  => $data['channel'],
+			'message'   => $data['message']
+		];
+
+		$statement = $this->pdo->prepare(sprintf('INSERT INTO %s (level, channel, message) VALUES (:level, :channel, :message)', $this->logs_table));
+		$statement->execute($fields);
+	}
+}
---
+++ tests/PDO.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * User: nvolgas
+ * Date: 11/24/14
+ * Time: 4:39 PM
+ */
+
+namespace volnix\Monolog\Tests;
+
+use Monolog\Logger;
+use volnix\Monolog\PDO\Handler;
+use PHPUnit\Framework\TestCase;
+
+class PDOTest extends TestCase {
+
+	const APP_NAME = 'pdo_handler_tests';
+	/**
+	 * @var null|\PDO $pdo
+	 */
+	private $pdo = null;
+
+	/**
+	 * @var null|\Monolog\Logger $logger
+	 */
+	private $logger = null;
+
+	protected function setUp()
+	{
+        // set our dsn and create our pdo instance
+        $dsn = sprintf('sqlite:%s/pdo_test.db', __DIR__);
+        $this->pdo = new \PDO($dsn);
+        $this->handler = new Handler($this->pdo);
+
+		// create our logger and our pdo handler
+		$this->logger = new Logger(self::APP_NAME);
+        $this->logger->pushHandler($this->handler);
+
+        // drop the table
+        $drop_sql = sprintf("
+			DROP TABLE IF EXISTS %s;
+		", Handler::DEFAULT_TABLE_NAME);
+
+		$this->pdo->exec($drop_sql);
+        
+        // create the table
+		$create_sql = sprintf("
+        CREATE TABLE %s (
+          id integer(11) PRIMARY KEY,
+          time datetime DEFAULT(CURRENT_TIMESTAMP),
+          level varchar(100) NOT NULL,
+          channel varchar(25) NOT NULL,
+          message text
+        );
+        ", Handler::DEFAULT_TABLE_NAME);
+
+        $this->pdo->exec($create_sql);
+	}
+
+	public function testSimpleError()
+	{
+        $this->logger->error('This is a test error');
+        
+        $row = $this->pdo->query(sprintf("select * from %s where channel = '%s'", Handler::DEFAULT_TABLE_NAME, self::APP_NAME))->fetch($this->pdo::FETCH_ASSOC);
+
+		$this->assertEquals(Logger::getLevelName(Logger::ERROR), $row['level']);
+		$this->assertEquals(self::APP_NAME, $row['channel']);
+		$this->assertEquals('This is a test error', $row['message']);
+    }
+}
+ 
